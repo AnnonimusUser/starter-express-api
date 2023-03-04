8a5bad25db9cf60c51f8881ef2d9bb6f9770b7e4
AnnonimusUser <126960038+AnnonimusUser@users.noreply.github.com>
2023-03-04 17:58:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo DiguBha!')
 })
+app.get('/clg',(req,res)=>{
+res.send("Yo it's /Clg")})
 app.listen(process.env.PORT || 3000)
